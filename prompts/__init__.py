57ff9434c0016c3b8d133ecf40d428e3ee4799c6
ranc5291-dotcom <ranc5291@gmail.com>
2026-04-15 16:50:04 +0530

    Delete prompts directory

--- prompts/__init__.py
+++
@@ -1,5 +0,0 @@
-# prompts/__init__.py
-from .extraction_prompt import extraction_prompt
-from .matching_prompt import matching_prompt
-from .scoring_prompt import scoring_prompt
-from .explanation_prompt import explanation_prompt
